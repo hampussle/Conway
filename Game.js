c52c553a41d2d202444e90f1527dd12a5f997055
hampussle <hlinderstam@gmail.com>
2021-08-30 18:23:40 +0200

    re-organized main file

--- Game.js
+++ Game.js
@@ -16,91 +16,6 @@ let grid
 let next
 let interval
 
-canvas.addEventListener('mousedown', function (e) {
-  let cords = getCords(e)
-  clickCell(cords.x, cords.y)
-  clicked = true
-})
-canvas.addEventListener('mouseup', function () {
-  clicked = false
-})
-canvas.addEventListener('mouseleave', function () {
-  clicked = false
-})
-canvas.addEventListener('mousemove', function (e) {
-  if (clicked) {
-    let cords = getCords(e)
-    clickCell(cords.x, cords.y)
-  }
-})
-slider.oninput = function () {
-  size = parseInt(slider.value)
-  sliderText.innerHTML = 'size: ' + size
-  stop()
-  setup()
-}
-function setup() {
-  updateCanvas()
-  grid = makeGrid()
-  makeRandomGrid()
-  draw()
-}
-function start() {
-  stop()
-  interval = setInterval(function () {
-    makeNext()
-  }, 100)
-  return interval
-}
-function updateCanvas() {
-  canvas.width = window.innerWidth
-  canvas.height =
-    window.innerHeight - document.getElementById('navbar').clientHeight
-  cols = Math.floor(canvas.height / size)
-  rows = Math.floor(canvas.width / size)
-}
-function stop() {
-  clearInterval(interval)
-  interval = null
-}
-function makeNext() {
-  clearCanvas()
-  updateCanvas()
-  next = makeGrid()
-  if (cols > grid.length) {
-    grid = makeGrid()
-    makeRandomGrid()
-  }
-  generations++
-  aliveCells = 0
-  nextGeneration()
-  grid = next
-  updateText()
-  draw()
-}
-function makeRandomGrid() {
-  generations = 0
-  for (let y = 0; y < cols; y++) {
-    for (let x = 0; x < rows; x++) {
-      grid[y][x] = Math.round(Math.random())
-    }
-  }
-}
-function rng() {
-  makeRandomGrid()
-  makeNext()
-}
-function draw() {
-  for (let y = 0; y < cols; y++) {
-    for (let x = 0; x < rows; x++) {
-      let i = y * size
-      let j = x * size
-      if (grid[y][x] == 1) {
-        drawCell(j, i)
-      }
-    }
-  }
-}
 function nextGeneration() {
   for (let y = 0; y < cols; y++) {
     for (let x = 0; x < rows; x++) {
@@ -117,6 +32,7 @@ function nextGeneration() {
     }
   }
 }
+
 function countAlive(y, x) {
   let alive = 0
   for (let i = -1; i < 2; i++) {
@@ -129,9 +45,49 @@ function countAlive(y, x) {
   }
   return alive
 }
-function clearCanvas() {
-  context.clearRect(0, 0, canvas.width, canvas.height)
+
+function draw() {
+  for (let y = 0; y < cols; y++) {
+    for (let x = 0; x < rows; x++) {
+      let i = y * size
+      let j = x * size
+      if (grid[y][x] == 1) {
+        drawCell(j, i)
+      }
+    }
+  }
 }
+
+function updateCanvas() {
+  canvas.width = window.innerWidth
+  canvas.height =
+    window.innerHeight - document.getElementById('navbar').clientHeight
+  cols = Math.floor(canvas.height / size)
+  rows = Math.floor(canvas.width / size)
+}
+
+function start() {
+  stop()
+  if (aliveCells == 0) rng()
+  interval = setInterval(function () {
+    makeNext()
+  }, 100)
+  return interval
+}
+
+function stop() {
+  clearInterval(interval)
+  interval = null
+}
+
+function setup() {
+  updateCanvas()
+  updateText()
+  grid = makeGrid()
+  makeRandomGrid()
+  draw()
+}
+
 function reset() {
   stop()
   clearCanvas()
@@ -140,6 +96,41 @@ function reset() {
   updateText()
   grid = emptyGrid()
 }
+
+function rng() {
+  makeRandomGrid()
+  makeNext()
+}
+
+function makeNext() {
+  clearCanvas()
+  updateCanvas()
+  next = makeGrid()
+  if (cols > grid.length) {
+    grid = makeGrid()
+    makeRandomGrid()
+  }
+  generations++
+  aliveCells = 0
+  nextGeneration()
+  grid = next
+  updateText()
+  draw()
+}
+
+function makeRandomGrid() {
+  generations = 0
+  for (let y = 0; y < cols; y++) {
+    for (let x = 0; x < rows; x++) {
+      grid[y][x] = Math.round(Math.random())
+    }
+  }
+}
+
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
 function makeGrid() {
   let arr = new Array(cols)
   for (let y = 0; y < cols; y++) {
@@ -147,6 +138,7 @@ function makeGrid() {
   }
   return arr
 }
+
 function emptyGrid() {
   let arr = makeGrid()
   for (let y = 0; y < cols; y++) {
@@ -156,6 +148,7 @@ function emptyGrid() {
   }
   return arr
 }
+
 function drawCell(x, y) {
   context.beginPath()
   context.rect(x, y, size, size)
@@ -163,32 +156,67 @@ function drawCell(x, y) {
   context.fill()
   context.stroke()
 }
+
 function getRandomColour() {
   let red = Math.floor(Math.random() * 255)
   let green = Math.floor(Math.random() * 255)
   let blue = Math.floor(Math.random() * 255)
   return 'rgb(' + red + ',' + green + ',' + blue + ' )'
 }
+
 function updateText() {
   cellsText.innerHTML = 'Alive cells: ' + aliveCells
   generationsText.innerHTML = 'Number of generations: ' + generations
 }
+
 function clickCell(x, y) {
   let cell = returnCellCords(x, y)
   let cellPos = returnCellPosition(cell)
+  if (grid[cell.y][cell.x] != 1) aliveCells++
   grid[cell.y][cell.x] = 1
   drawCell(cellPos.x, cellPos.y)
 }
+
 function returnCellCords(x, y) {
   let row = Math.floor(x / size)
   let col = Math.floor(y / size)
   return { x: row, y: col }
 }
+
 function returnCellPosition(cell) {
   let row = cell.x * size
   let col = cell.y * size
   return { x: row, y: col }
 }
+
 function getCords(event) {
   return { x: event.offsetX, y: event.offsetY }
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  let cords = getCords(e)
+  clickCell(cords.x, cords.y)
+  clicked = true
+})
+
+canvas.addEventListener('mouseup', () => {
+  clicked = false
+})
+
+canvas.addEventListener('mouseleave', () => {
+  clicked = false
+})
+
+canvas.addEventListener('mousemove', (e) => {
+  if (clicked) {
+    let cords = getCords(e)
+    clickCell(cords.x, cords.y)
+  }
+})
+
+slider.oninput = () => {
+  size = parseInt(slider.value)
+  sliderText.innerHTML = 'size: ' + size
+  stop()
+  setup()
 }
